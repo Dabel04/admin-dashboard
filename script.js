f9efb36a43d1b81256816192adf07619aabf5431
dabel <iyiegbudabelchi16@gmail.com>
2025-11-16 22:24:11 +0100

    fixed

--- script.js
+++ script.js
@@ -6,6 +6,8 @@
     const sidebar = document.getElementById('sidebar');
     const mainContent = document.getElementById('mainContent');
     const logoutBtn = document.getElementById('logoutBtn');
+    const header = document.getElementById('header');
+    const overview = document.getElementById('overview');
 
     const validUsername = "makecodelit@gmail.com";
     const validPassword = "ilovecoding123";
@@ -33,6 +35,10 @@
     sidebar.classList.toggle('bg-dark');
       sidebar.classList.toggle('text-white');
       mainContent.style.backgroundColor = isDarkMode ? '#343a40' : '#fff';
+      header.style.backgroundColor = isDarkMode ? '#f7f4f3' : '#5b2333';
+       header.style.color = isDarkMode ? '#343a40' : '#f7f4f3';
+        overview.style.backgroundColor = isDarkMode ? '#f7f4f3' : '#5b2333';
+       overview.style.color = isDarkMode ? '#343a40' : '#f7f4f3';
       mainContent.style.color = isDarkMode ? '#fff' : '#000';
       toggleBtn.textContent = isDarkMode ? 'Toggle Light Mode' : 'Toggle Dark Mode';
     });
